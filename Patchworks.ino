eed1c5acde0c8c776dda031333dcc66efa800723
Jeffrey Bednar <jeffreybednar@live.ca>
2026-03-13 13:24:52 -0400

    Formatting

--- Patchworks.ino
+++ Patchworks.ino
@@ -8,7 +8,7 @@
 // This project is provided for educational and learning purposes only. You are welcome to read, study, and experiment     //
 // with this software and/or hardware. It is not intended for commercial use. This software and/or hardware is provided    //
 // "as is", without warranty of any kind. The author assumes no responsibility for any damages or issues resulting from    //
-// its use.                     																						                                               //
+// its use.                                                                                                                //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Date: Tuesday, October 3rd, 2023                                                                                        
 // Description: Source for the Patchworks electronics prototyping and monitoring board.                                    
